e8617219934d09ea8a7afbd06974213dc06a20af
Joe Greenwood <grnwood@gmail.com>
2022-06-01 08:19:18 -0500

    added file support for extra urls

--- biscuit.js
+++ biscuit.js
@@ -1,11 +1,12 @@
 const Sitemapper = require('sitemapper')
 const http = require('http')
 const { nettime, getDuration } = require('nettime')
+const fs = require('fs');
 var colors = require('colors');
 var limit = 0
 var random = false
 
-if (process.argv.length < 3 ) {
+if (process.argv.length < 3) {
     console.log('biscuit.js')
     console.log('---=======-----')
     console.log('Crawl sitemaps and get some numbers.')
@@ -14,44 +15,53 @@ if (process.argv.length < 3 ) {
     console.log('')
     console.log("optional:")
     console.log(" --limit=X\t # limit to X URLs regardless of sitemap entries")
+    console.log(" --infile\t # extra urls to target, full url one on each line")
     console.log(" --randomize\t # randomize URLs out of the sitemap")
     console.log(" --user=<user> --password=<pass> if needed.")
+
     process.exit(1)
 }
 
 var siteToCrawl = process.argv[2]
-var limitURLs = process.argv[3] != undefined ? process.argv[3]: undefined
-var randomURLs = process.argv[4] != undefined ? process.argv[4]: undefined
-var user, password
+var limitURLs = process.argv[3] != undefined ? process.argv[3] : undefined
+var randomURLs = process.argv[4] != undefined ? process.argv[4] : undefined
+var user, password, filename
 
-process.argv.forEach(function (value,index) {
-    if (value.indexOf('http') >=0) {
+process.argv.forEach(function (value, index) {
+    if (value.indexOf('http') >= 0) {
         siteToCrawl = value
     }
-    if (value.indexOf('--limit')>=0) {
+    if (value.indexOf('--limit') >= 0) {
         splitval = limitURLs.split('=')
         if (splitval[1]) {
             limit = Math.floor(splitval[1])
         }
     }
-    if (value.indexOf('randomize') >=0) {
+    if (value.indexOf('randomize') >= 0) {
         random = true
         console.error("!  Randomize not implemented yet")
         process.exit(1)
     }
-    if (value.indexOf('--user') >=0) {
+    if (value.indexOf('--user') >= 0) {
         splitval = value.split('=')
         if (splitval[1]) {
             user = splitval[1]
         }
     }
-    if (value.indexOf('--password') >=0) {
+    if (value.indexOf('--password') >= 0) {
         splitval = value.split('=')
         if (splitval[1]) {
             password = splitval[1]
         }
     }
-    
+    if (value.indexOf('--infile') >= 0) {
+        splitval = value.split('=')
+        if (splitval[1]) {
+            filename = splitval[1]
+        }
+    }
+
+
 })
 
 var waitTotal = 0
@@ -60,23 +70,27 @@ var processed = 0
 var errors = 0
 var lastone = false
 
-console.log(String("! Parsing sitemap "+siteToCrawl).cyan)
+console.log(String("! Parsing sitemap " + siteToCrawl).cyan)
 
 const sitemapper = new Sitemapper({
     url: siteToCrawl,
     timeout: 15000,
     requestHeaders: {
-      'Authorization': getCredsBasicAuthHeader(user, password)
+        'Authorization': getCredsBasicAuthHeader(user, password)
     },
     debug: true,
     concurrency: 2,
     retries: 1,
-  });
+});
 
 sitemapper.fetch(siteToCrawl).then(function (sites) {
 
+    if (filename) {
+        urls = getExtraUrls(filename)
+        console.log(String('\t\t ! Adding ' + urls.length + ' extra urls from ' + filename).magenta)
+    }
     if (limit) {
-        console.log(String("\t\t ! Limiting urls to "+limit+ ' URLs').magenta)
+        console.log(String("\t\t ! Limiting urls to " + limit + ' URLs').magenta)
     }
 
     if (random) {
@@ -89,36 +103,44 @@ sitemapper.fetch(siteToCrawl).then(function (sites) {
     }
 
     console.log(String("\t\t ! Site has " + sites.sites.length + " urls to warm up ...").cyan)
-    if ( limit > 0 && limit < sites.sites.length) {
+    if (limit > 0 && limit < sites.sites.length) {
         // just grab limit for now
-        toWorkOn = sites.sites.slice(0,limit)
+        toWorkOn = sites.sites.slice(0, limit)
         processedTotal = limit
-        console.log(String("! \t\t\t Limiting to "+limit+" crawls").red)
+        console.log(String("! \t\t\t Limiting to " + limit + " crawls").red)
     } else {
         toWorkOn = sites.sites
         processedTotal = sites.sites.length
     }
-    
-    console.log("\t! Queueing up URLs.. #".grey)
-    toWorkOn.forEach(function(item, index) {
-        process.stdout.write("#")
-        credentials = {username: user, password: password}
 
-        console.log("*")
-        nettime({url: item, followRedirects: true, timeout: 15000, 
-            credentials: credentials}).then(result => {
-            processResult(item, result)  
+    if (urls) {
+        urls.forEach(function (v, i) {
+            if (v) {
+                toWorkOn.unshift(v)
+            }
         })
-        .catch(error =>  { 
-            console.error('! '+ item + ': '+ error.message)
-            processed += 1
-            errors += 1
-        }
-        )
+    }
+
+    console.log("\t! Queueing up URLs.. #".grey)
+    toWorkOn.forEach(function (item, index) {
+        credentials = { username: user, password: password }
+
+        nettime({
+            url: item, followRedirects: true, timeout: 30000,
+            credentials: credentials
+        }).then(result => {
+            processResult(item, result)
+        })
+            .catch(error => {
+                console.error('! ' + item + ': ' + error.message)
+                processed += 1
+                errors += 1
+            }
+            )
     })
-  
+
     console.log('! DONE queueing'.green)
-    
+
 })
 
 function processResult(item, result) {
@@ -131,7 +153,7 @@ function processResult(item, result) {
     }
 
     //console.log(toProcess)
-    console.log(String("! "+item+"\t ("+toProcess.statusCode+"/"+toProcess.statusMessage+")").gray)
+    console.log(String("! " + item + "\t (" + toProcess.statusCode + "/" + toProcess.statusMessage + ")").gray)
 
     let timings = toProcess.timings
     let waiting = getDuration([0, 0], timings.firstByte)
@@ -139,18 +161,18 @@ function processResult(item, result) {
     let downloading = getDuration(timings.firstByte, timings.contentTransfer)
     //console.log(item)
     console.log('\twt:' + threeDig(nettime.getMilliseconds(waiting)) + 'ms' + '\tdl:' + threeDig(nettime.getMilliseconds(downloading)) + 'ms'
-        +'\t\t TOTAL wt '+threeDig(waitTotal)+ '  ('+threeDig(waitTotal/processed)+' avg ms)' + String('\t Count: '+processed + ' of ' +processedTotal).red)
+        + '\t\t TOTAL wt ' + threeDig(waitTotal) + '  (' + threeDig(waitTotal / processed) + ' avg ms)' + String('\t Count: ' + processed + ' of ' + processedTotal).red)
 
-    if (processed >= processedTotal-1) {
+    if (processed >= processedTotal - 1) {
         if (!lastone) {
             lastone = true
             console.log('! biscuit is done.'.green)
-            console.log(String('\t TOTAL wt: '+threeDig(waitTotal)+ '  ('+threeDig(waitTotal/processed)+' avg ms)' + String('\t Processed Count: '+processedTotal)).cyan)
-            console.log(String('! \t\t\t Of ' +processedTotal+' there were ' + errors + ' errors. '+(errors/processedTotal*100)+'% error rate').cyan)
+            console.log(String('\t TOTAL wt: ' + threeDig(waitTotal) + '  (' + threeDig(waitTotal / processed) + ' avg ms)' + String('\t Processed Count: ' + processedTotal)).cyan)
+            console.log(String('! \t\t\t Of ' + processedTotal + ' there were ' + errors + ' errors. ' + (errors / processedTotal * 100) + '% error rate').cyan)
         } else {
             // do nothing, async issue?
         }
-        
+
 
     }
 }
@@ -162,6 +184,17 @@ function threeDig(inno) {
 function getCredsBasicAuthHeader(user, pass) {
     const token = Buffer
         .from(`${user}:${pass}`)
-    .toString('base64')
-        return `Basic ${token}`
+        .toString('base64')
+    return `Basic ${token}`
+}
+
+function getExtraUrls(filename) {
+    try {
+        const data = fs.readFileSync(filename, 'utf8');
+        if (data) {
+            return data.split('\r\n')
+        }
+    } catch (err) {
+        console.error(err);
+    }
 }
